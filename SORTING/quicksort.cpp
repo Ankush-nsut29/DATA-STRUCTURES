5db1ea8d209f25a198b815a1f5a008faeeb246d8
Ankush-nsut29 <ak2024iit@gmail.com>
2026-04-17 22:32:49 +0530

    Create quicksort.cpp

---
+++ SORTING/quicksort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort(int arr[],int size){
+    
+}
+
+
+void display(int arr[],int size){
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int a[4]={1,4,3,2};
+    sort(a,4);
+    display(a,4);
+}

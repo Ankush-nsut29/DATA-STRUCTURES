3b14cdb6192a77582a29693c7c7802d38436d52e
Ankush-nsut29 <ak2024iit@gmail.com>
2026-05-17 14:41:14 +0530

    Update HEAPSORTARRAYS.cpp

--- SORTING/HEAPSORTARRAYS.cpp
+++ SORTING/HEAPSORTARRAYS.cpp
@@ -6,15 +6,15 @@ void heapify(int arr[], int size, int i) {
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    
+    //checks if left child is larget than part
     if(left < size && arr[left] > arr[largest]) {
         largest = left;
     }
-    
+    //checks if right child is larger than parent 
     if(right < size && arr[right] > arr[largest]) {
         largest = right;
     }
-    
+    //if largest was switched by left or right then swap and use haepify to fix any anomoly again
     if(largest != i) {
         swap(arr[i], arr[largest]);
         heapify(arr, size, largest);
@@ -22,11 +22,12 @@ void heapify(int arr[], int size, int i) {
 }
 
 void heapsort(int arr[], int size) {
-
+//first make any simple array a heap
     for(int i = size / 2 - 1; i >= 0; i--) {
         heapify(arr, size, i);
     }
-    
+//now what we do is witch first means largest element weith the last one now largest is at last
+//and use heapify to make the array heap again
     for(int i = size - 1; i > 0; i--) {
         swap(arr[0], arr[i]);
         heapify(arr, i, 0);
